e55595977b29d73848d9929fd8048b1435a01fd1
Andres De Biase <25203394+ardba@users.noreply.github.com>
2022-04-24 07:57:35 +0200

    Updated for new ProtonExperimental

--- run.sh
+++ run.sh
@@ -12,6 +12,8 @@ echo "Fetching vc_redist.x64.exe"
 wget https://download.microsoft.com/download/0/6/4/064F84EA-D1DB-4EAA-9A5C-CC2F0FF6A638/vc_redist.x64.exe
 cabextract vc_redist.x64.exe
 cabextract a10
-cp ucrtbase.dll  /home/$SUDO_USER/.local/share/Steam/steamapps/compatdata/1493710/pfx/drive_c/windows/system32/ # This changes sometimes, depending on proton version?
+
+rm /home/$SUDO_USER/.local/share/Steam/steamapps/compatdata/813780/pfx/drive_c/windows/system32/ucrtbase.dll # There might be a simlink to the "default" ucrtbase.dll, it needs to be deleted
+cp ucrtbase.dll  /home/$SUDO_USER/.local/share/Steam/steamapps/compatdata/813780/pfx/drive_c/windows/system32/ # This changes sometimes, depending on proton version?
 echo "Copied dll"
-rm -rf $folder
+
