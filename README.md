c4d8da861a83b54b1f09f6efc95d59d6e6974c18
Andres De Biase <25203394+ardba@users.noreply.github.com>
2022-05-03 20:41:01 +0200

    Copied changes from mabster314's fork

---
+++ README.md
@@ -0,0 +1,2 @@
+# Multiplayer fix for AoE 2: DE
+This script checks if ucrtbase.dll is installed for AoE2: DE then installs it if necessary. To run automatically before launching AoE, set your Steam launch options to `location/of/script/run.sh ; %command%`
--- run.sh
+++ run.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 set -e # Fail fast
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit 1
+STEAM_LOCATION=~/.local/share
+SYSTEM32_LOCATION=$STEAM_LOCATION/Steam/steamapps/compatdata/1493710/pfx/drive_c/windows/system32/ # This changes sometimes, depending on proton version?
+
+# Check if the dll already exists
+if [ -f "$SYSTEM32_LOCATION/ucrtbase.dll" ]; then
+    echo "ucrtbase.dll is already installed"
+    exit 1
 fi
 
 folder="/tmp/fix-aoe"
-mkdir -p $folder && cd "$_"
+mkdir -p  && cd "--color=auto"
 echo "Fetching vc_redist.x64.exe"
 wget https://download.microsoft.com/download/0/6/4/064F84EA-D1DB-4EAA-9A5C-CC2F0FF6A638/vc_redist.x64.exe
 cabextract vc_redist.x64.exe
 cabextract a10
-
-rm /home/$SUDO_USER/.local/share/Steam/steamapps/compatdata/813780/pfx/drive_c/windows/system32/ucrtbase.dll # There might be a simlink to the "default" ucrtbase.dll, it needs to be deleted
-cp ucrtbase.dll  /home/$SUDO_USER/.local/share/Steam/steamapps/compatdata/813780/pfx/drive_c/windows/system32/ # This changes sometimes, depending on proton version?
+cp ucrtbase.dll $SYSTEM32_LOCATION
 echo "Copied dll"
-
+rm -rf 
