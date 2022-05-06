00ed68f9e0b69895be4dbd6fd60a5bcf2abcb3d5
Roland Vet <vet.roland@gmail.com>
2022-05-06 23:48:47 +0200

    Use a dynamically allocated temporary folder
    and cleanup afterwards.

--- run.sh
+++ run.sh
@@ -12,13 +12,15 @@ fi
 
 rm -f $SYSTEM32_LOCATION/ucrtbase.dll # There might be a simlink to the "default" ucrtbase.dll, it needs to be deleted
 
-folder="/tmp/fix-aoe"
-mkdir -p $folder && cd "$_"
+tempFolder=$(mktemp --directory)
+cd ${tempFolder}
 echo "Fetching vc_redist.x64.exe"
 wget https://download.microsoft.com/download/0/6/4/064F84EA-D1DB-4EAA-9A5C-CC2F0FF6A638/vc_redist.x64.exe
 cabextract vc_redist.x64.exe --filter a10
 cabextract a10 --filter ucrtbase.dll
 cp ucrtbase.dll $SYSTEM32_LOCATION
 echo "Copied ucrtbase.dll"
+rm -f vc_redist.x64.exe a10 ucrtbase.dll
+rmdir ${tempFolder}
 
 exit 0
