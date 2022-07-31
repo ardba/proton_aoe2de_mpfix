8f91fe7ab9d38512e90cd614930b21b5cdbdf8ab
Andres De Biase <25203394+ardba@users.noreply.github.com>
2022-07-31 16:10:26 +0200

    Incorporated changes from pr#8 & renamed variable to match the rest of script

--- run.sh
+++ run.sh
@@ -9,7 +9,14 @@ else
     APPID=813780 # The most recent App ID when this was written
 fi
 
-STEAM_LOCATION=/home/$USER/.local/share
+STEAM_LOCATION="/home/$USER/.local/share"
+
+# check if arguments are not empty
+if [ ! $# -eq 0 ]; then
+    # set STEAM_LOCATION to the provided argument
+    STEAM_LOCATION=$1
+fi
+
 SYSTEM32_LOCATION=$STEAM_LOCATION/Steam/steamapps/compatdata/$APPID/pfx/drive_c/windows/system32
 
 # Check if the dll already exists and is not a symlink
@@ -20,8 +27,8 @@ fi
 
 rm -f $SYSTEM32_LOCATION/ucrtbase.dll # There might be a simlink to the "default" ucrtbase.dll, it needs to be deleted
 
-tempFolder=$(mktemp --directory)
-cd ${tempFolder}
+TEMP_FOLDER=$(mktemp --directory)
+cd $TEMP_FOLDER
 echo "Fetching vc_redist.x64.exe"
 wget https://download.microsoft.com/download/0/6/4/064F84EA-D1DB-4EAA-9A5C-CC2F0FF6A638/vc_redist.x64.exe
 cabextract vc_redist.x64.exe --filter a10
@@ -29,6 +36,6 @@ cabextract a10 --filter ucrtbase.dll
 cp ucrtbase.dll $SYSTEM32_LOCATION
 echo "Copied ucrtbase.dll"
 rm -f vc_redist.x64.exe a10 ucrtbase.dll
-rmdir ${tempFolder}
+rmdir $TEMP_FOLDER
 
 exit 0
